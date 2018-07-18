640b08a6c0e8cd0e032f9f0fef27790f2e68e815
songzy <songzy.thu@qq.com>
2018-07-19 01:52:21 +0800

    bug fix: embed_size

--- src/common.py
+++ src/common.py
@@ -6,6 +6,6 @@ test_path = '../input/cikm_test_a_20180516.txt'
 en_vec_path = '../input/wiki.en.vec'
 es_vec_path = '../input/wiki.es.vec'
 
-embed_size = 512 
+embed_size = 300 
 max_features = 5000
 maxlen = 54
